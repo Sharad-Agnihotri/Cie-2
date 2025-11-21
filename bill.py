6d058ce6ae651b9ff0fe5e06289ccbcfcd20c3e1
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2025-11-21 10:00:24 +0530

    save

---
+++ bill.py
@@ -0,0 +1,10 @@
+import sys
+if len(sys.argv)>3:
+    qty=sys.argv[1]
+    price_per_item=sys.argv[2]
+    print("Invalid parameters")
+else:
+    qty=2
+    price_per_item=100
+total_bill=qty*price_per_item
+print("Total bill is:",total_bill)
