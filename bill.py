62de85f38928e62b87748134d152a0cc500acbfb
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2025-11-21 10:58:08 +0530

    save

--- bill.py
+++ bill.py
@@ -1,10 +1,13 @@
 import sys
-if len(sys.argv)>3:
-    qty=sys.argv[1]
-    price=sys.argv[2]
-    print("Invalid parameters")
+
+
+if len(sys.argv) == 3:
+    qty = int(sys.argv[1])
+    price = float(sys.argv[2])
 else:
-    qty=12
-    price=100
-total_bill=qty*price
-print("Total bill is:",total_bill)
+    
+    qty = 12
+    price = 100
+
+total_bill = qty * price
+print("Total bill is:", total_bill)
